7f3aa88fe7f38267d53e870198e36289258b7824
常国杰 <mr.chang404@qq.com>
2022-04-09 17:00:54 +0800

    更新sql文件

--- RBAC/rbac.sql
+++ RBAC/rbac.sql
@@ -1,25 +1,6 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : localhost_3306
- Source Server Type    : MySQL
- Source Server Version : 80023
- Source Host           : localhost:3306
- Source Schema         : rbac
-
- Target Server Type    : MySQL
- Target Server Version : 80023
- File Encoding         : 65001
-
- Date: 09/04/2022 16:57:36
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
--- ----------------------------
--- Table structure for menu
--- ----------------------------
 DROP TABLE IF EXISTS `menu`;
 CREATE TABLE `menu`  (
   `id` int(0) NOT NULL COMMENT '主键',
@@ -29,16 +10,10 @@ CREATE TABLE `menu`  (
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of menu
--- ----------------------------
 INSERT INTO `menu` VALUES (0, '公共资源', '所有人可以访问的资源', '/getVerificationCode.do');
 INSERT INTO `menu` VALUES (1, '用户菜单', '用户可以进行操作的菜单', '/hello');
 INSERT INTO `menu` VALUES (2, '系统菜单', '管理员可以进行的操作', '/*');
 
--- ----------------------------
--- Table structure for role
--- ----------------------------
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role`  (
   `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '角色id',
@@ -47,32 +22,21 @@ CREATE TABLE `role`  (
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of role
--- ----------------------------
+
 INSERT INTO `role` VALUES (0, 'Role_visitor', '访客');
 INSERT INTO `role` VALUES (1, 'Role_admin', '系统管理员');
 INSERT INTO `role` VALUES (2, 'Role_user', '用户');
 
--- ----------------------------
--- Table structure for role_menu
--- ----------------------------
 DROP TABLE IF EXISTS `role_menu`;
 CREATE TABLE `role_menu`  (
   `mid` int(0) NOT NULL COMMENT 'menu的主键',
   `rid` int(0) NOT NULL COMMENT 'role的主键'
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of role_menu
--- ----------------------------
 INSERT INTO `role_menu` VALUES (0, 0);
 INSERT INTO `role_menu` VALUES (2, 2);
 INSERT INTO `role_menu` VALUES (2, 1);
 
--- ----------------------------
--- Table structure for user
--- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
   `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '主键',
@@ -85,15 +49,9 @@ CREATE TABLE `user`  (
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of user
--- ----------------------------
 INSERT INTO `user` VALUES (1, 'root', '{noop}123', 1, 1, 1, 1);
 INSERT INTO `user` VALUES (2, 'iguojie', '{noop}123', 1, 1, 1, 1);
 
--- ----------------------------
--- Table structure for user_role
--- ----------------------------
 DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE `user_role`  (
   `uid` int(0) NULL DEFAULT NULL COMMENT 'user主键',
@@ -102,15 +60,9 @@ CREATE TABLE `user_role`  (
   INDEX `rid`(`rid`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of user_role
--- ----------------------------
 INSERT INTO `user_role` VALUES (1, 1);
 INSERT INTO `user_role` VALUES (2, 2);
 
--- ----------------------------
--- View structure for 权限一览
--- ----------------------------
 DROP VIEW IF EXISTS `权限一览`;
 CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `权限一览` AS select `user`.`username` AS `用户名`,`role`.`name_zh` AS `权限名称中文`,`menu`.`name` AS `菜单名称`,`role`.`name` AS `权限名称`,`menu`.`url` AS `url` from ((((`user` join `user_role` on((`user`.`id` = `user_role`.`uid`))) join `role` on((`user_role`.`rid` = `role`.`id`))) join `role_menu` on((`role`.`id` = `role_menu`.`mid`))) join `menu` on((`role_menu`.`mid` = `menu`.`id`)));
 
